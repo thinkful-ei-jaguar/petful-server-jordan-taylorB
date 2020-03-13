4e6edd537097f867be4fde71e9c14affc0f6ee2d
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-03-13 14:52:35 -0400

    touch ups on get methods, basic set up for deploy

--- app/app.js
+++ app/app.js
@@ -3,7 +3,9 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}))
 
 app.use('/api/people', require('../people/people.router'))
 app.use('/api/pets', require('../pets/pets.router'))
