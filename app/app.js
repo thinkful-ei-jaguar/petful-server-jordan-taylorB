fc5146de5a883e45489c55a76e3e74e8cec7dcac
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-13 12:37:34 -0700

    removed node modules

--- app/app.js
+++ app/app.js
@@ -3,9 +3,11 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors({
-    origin: CLIENT_ORIGIN
-}))
+app.use(cors(
+    // {
+// origin: CLIENT_ORIGIN
+// }
+))
 
 app.use('/api/people', require('../people/people.router'))
 app.use('/api/pets', require('../pets/pets.router'))
