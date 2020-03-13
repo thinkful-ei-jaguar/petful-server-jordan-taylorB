6d912746acbd9f08f9e6e1609a3fc4479c151f17
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-13 14:02:45 -0700

    fixed node modules and package-lock

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors(
-    // {
+  // {
 // origin: CLIENT_ORIGIN
 // }
 ))
@@ -13,14 +13,14 @@ app.use('/api/people', require('../people/people.router'))
 app.use('/api/pets', require('../pets/pets.router'))
 
 app.use('/', (req, res) => {
-    let response;
-    if (NODE_ENV === 'production') {
-        response = {error: {messge: 'server error'}}
-    }
-    else {
-        respone = {messge: error.message, error}
-    }
-    res.status(500).json(response)
+  let response;
+  if (NODE_ENV === 'production') {
+    response = {error: {messge: 'server error'}}
+  }
+  else {
+    response = {message: error.message, error}
+  }
+  res.status(500).json(response)
 })
 
 module.exports = app
--- pets/pets.router.js
+++ pets/pets.router.js
@@ -10,28 +10,28 @@ router
   .route('/cat')
   .get((req, res) => {
   // Return all cats currently up for adoption.
-  const results = Pets.getCats()
+    const results = Pets.getCats()
   
-  if(results === null) {
-    res.send('There are no cats available at this time')
-  }
+    if(results === null) {
+      res.send('There are no cats available at this time')
+    }
 
-
-  res.json(results[0])
-})
+    console.log(results[0]);
+    return res.json(results[0])
+  })
 
 router
   .route('/dog')
   .get((req, res) => {
   // Return all dogs currently up for adoption.
-  const results = Pets.getDogs()
+    const results = Pets.getDogs()
 
-  if(results === null) {
-    res.send('There are no cats available at this time')
-  }
+    if(results === null) {
+      res.send('There are no cats available at this time')
+    }
 
-  res.json(results[0])
-})
+    res.json(results[0])
+  })
 
 router.delete('/', bodyParser, (req, res) => {
   // Remove a pet from adoption.
