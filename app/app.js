d8d5816e0dd3b56107c3a1adfe44abfe22bb72a6
Jordan Castillo <jordanxcallaway@gmail.com>
2020-04-11 12:19:23 -0700

    setting up cors

--- app/app.js
+++ app/app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors(
-  // {
-// origin: CLIENT_ORIGIN
-// }
-));
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
 
-app.use('/api/people', require('../people/people.router'));
-app.use('/api/pets', require('../pets/pets.router'));
+app.use("/api/people", require("../people/people.router"));
+app.use("/api/pets", require("../pets/pets.router"));
 
 module.exports = app;
